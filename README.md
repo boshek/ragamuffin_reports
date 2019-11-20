08dfc705390ce5c604efb3e611d9355f895aa8b6
boshek <sam.albers@gmail.com>
2019-11-19 22:03:24 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## ragamuffin reports
+
+A repo to hold code to create reports for appearances on the ragamuffin parage.
+
+## Usage 
+
+Run code in the `run_report.R` file
---
+++ run_report.R
@@ -0,0 +1,7 @@
+## 11/22/2019
+
+rmarkdown::render("ragamuffin_report_template.Rmd", params = list(
+  artist = 'kris kristofferson',
+  song1 = 'Highwayman',
+  song2 = 'I May Smoke Too Much'
+))
