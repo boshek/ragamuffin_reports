33b861405f27f42a3a0e68f956e4b5602f5b1c99
boshek <sam.albers@gmail.com>
2019-11-28 21:10:39 -0800

    update template.

--- run_report.R
+++ run_report.R
@@ -1,7 +1,22 @@
 ## 11/22/2019
 
-rmarkdown::render("ragamuffin_report_template.Rmd", params = list(
-  artist = 'kris kristofferson',
-  song1 = 'Highwayman',
-  song2 = 'I May Smoke Too Much'
-))
+rmarkdown::render("ragamuffin_report_template.Rmd",
+                  params = list(
+                    artist = 'kris kristofferson',
+                    song1 = 'Highwayman',
+                    song2 = 'I May Smoke Too Much'
+                    ),
+                  output_file = "reports/kristofferson_11-22-2019.pdf"
+                  )
+
+## 11/29/2019
+rmarkdown::render("ragamuffin_report_template.Rmd",
+                  params = list(
+                    artist = 'merle haggard',
+                    song1 = 'Mama Tried',
+                    song2 = 'Stay a Little Longer',
+                    song1id = '04ilBEd1FVUhpv9ItR6yTN',
+                    song2id = '0A1RBRUA6NGehkbByKBpNP'
+                  ),
+                  output_file = "reports/haggard_11-29-2019.pdf"
+)
